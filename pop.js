3418af655cc96d8f63354f9af30a9ae3ba8b1a59
mr coder <arbis@gmail.com>
2018-04-21 04:24:49 +0530

    first commit

---
+++ pop.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+        		$('.html').animate({width:'80%'},2000);
+        		$('.css').animate({width:'82%'},2000);
+        		$('.js').animate({width:'80%'},2000);
+        		$('.php').animate({width:'83%'},2000);
+        		$('.ajax').animate({width:'85%'},2000);
+        	});
